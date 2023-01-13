1181d773e58d92de1f5895d1d3e071b7d76f200a
Kacper Kilianek <01151053@pw.edu.pl>
2023-01-13 14:47:42 +0100

    Java code refractoring and repository cleanup

--- simple-emp-app-master/simple-emp-app-master/src/EmpApp.java
+++ simple-emp-app-master/simple-emp-app-master/src/EmpApp.java
@@ -1,87 +1,84 @@
-import java.io.FileInputStream;
-import java.io.*;
-import java.sql.*;
-import java.util.Random;
-import java.util.Scanner;
-import java.util.Properties;
+import oracle.jdbc.pool.OracleDataSource;
 
-import oracle.jdbc.pool.OracleDataSource; //sterownik bazy danych Oracle
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
 
 public class EmpApp {
 
-//	String filepath
-	Connection conn; // obiekt Connection do nawiazania polaczenia z baza danych
+    //	String filepath
+    Connection conn; // obiekt Connection do nawiazania polaczenia z baza danych
 
-	public static void main(String[] args) {
-		EmpApp app = new EmpApp();
+    public static void main(String[] args) {
+        EmpApp app = new EmpApp();
 
-		try {
-			app.setConnection(); // otwarcie polaczenia z BD
-			app.showEmployeesWithSalary(); // test polaczenia
-			app.closeConnection();// zamkniecie polaczenia z BD
-		} 
-		catch (SQLException eSQL) {
-			System.out.println("Blad przetwarzania SQL " + eSQL.getMessage());
-		}
-		catch (IOException eIO) {
-			System.out.println("Nie mozna otworzyc pliku" );
-		}
-	}
+        try {
+            app.setConnection(); // otwarcie polaczenia z BD
+            app.showEmployeesWithSalary(); // test polaczenia
+            app.closeConnection();// zamkniecie polaczenia z BD
+        } catch (SQLException eSQL) {
+            System.out.println("Blad przetwarzania SQL " + eSQL.getMessage());
+        } catch (IOException eIO) {
+            System.out.println("Nie mozna otworzyc pliku");
+        }
+    }
 
-	public void setConnection() throws SQLException, IOException { // metoda nawiazuje polaczenie
+    public void setConnection() throws SQLException, IOException { // metoda nawiazuje polaczenie
 
-		File myObj = new File("connection.properties");
-		String pathToProperties = myObj.getAbsolutePath();
-		pathToProperties = pathToProperties.replace("connection.properties", "simple-emp-app-master\\\\connection.properties");
-		pathToProperties = pathToProperties.replace("\\", "\\\\");
-		
-		Properties prop = new Properties();
-		FileInputStream in = new FileInputStream(pathToProperties); // w pliku znajduja sie parametry polaczenia
-		prop.load(in); // zaczytanie danych z pliku properties
-		in.close(); // zamkniecie pliku
+        File myObj = new File("connection.properties");
+        String pathToProperties = myObj.getAbsolutePath();
+        pathToProperties = pathToProperties.replace("connection.properties", "simple-emp-app-master\\simple-emp-app-master\\connection.properties");
+        pathToProperties = pathToProperties.replace("\\", "\\\\");
 
-		String host = prop.getProperty("jdbc.host");
-		String username = prop.getProperty("jdbc.username");
-		String password = prop.getProperty("jdbc.password");
-		String port = prop.getProperty("jdbc.port");
-		String serviceName = prop.getProperty("jdbc.service.name");
+        Properties prop = new Properties();
+        FileInputStream in = new FileInputStream(pathToProperties); // w pliku znajduja sie parametry polaczenia
+        prop.load(in); // zaczytanie danych z pliku properties
+        in.close(); // zamkniecie pliku
 
-		String connectionString = String.format(
-									"jdbc:oracle:thin:%s/%s@//%s:%s/%s",
-									username, password, host, port, serviceName);
+        String host = prop.getProperty("jdbc.host");
+        String username = prop.getProperty("jdbc.username");
+        String password = prop.getProperty("jdbc.password");
+        String port = prop.getProperty("jdbc.port");
+        String serviceName = prop.getProperty("jdbc.service.name");
 
-		System.out.println (connectionString);
-		OracleDataSource ods; // nowe zrodlo danych (klasa z drivera  Oracle)
-		ods = new OracleDataSource();
+        String connectionString = String.format(
+                "jdbc:oracle:thin:%s/%s@//%s:%s/%s",
+                username, password, host, port, serviceName);
 
-		ods.setURL(connectionString);
-		conn = ods.getConnection(); // nawiazujemy polaczenie z BD
+        System.out.println(connectionString);
+        OracleDataSource ods; // nowe zrodlo danych (klasa z drivera  Oracle)
+        ods = new OracleDataSource();
 
-		DatabaseMetaData meta = conn.getMetaData();
+        ods.setURL(connectionString);
+        conn = ods.getConnection(); // nawiazujemy polaczenie z BD
 
-		System.out.println("Polaczenie do bazy danych nawiaane.");
-		System.out.println("Baza danych:" + " " + meta.getDatabaseProductVersion());
-	}
+        DatabaseMetaData meta = conn.getMetaData();
 
-	public void closeConnection() throws SQLException { // zamkniecie polaczenia
-		conn.close();
-		System.out.println("Polaczenie z baza zamkniete poprawnie.");
-	}
+        System.out.println("Polaczenie z baza danych nawiazane.");
+        System.out.println("Baza danych:" + " " + meta.getDatabaseProductVersion());
+    }
 
-	public void showEmployeesWithSalary() throws SQLException {
-		System.out.println("Lista pracowników:");
+    public void closeConnection() throws SQLException { // zamkniecie polaczenia
+        conn.close();
+        System.out.println("Polaczenie z baza zamkniete poprawnie.");
+    }
 
-		Statement stat = conn.createStatement(); // Statement przechowujacy polecenie SQL
+    public void showEmployeesWithSalary() throws SQLException {
+        System.out.println("Lista pracowników:");
 
-		// wydajemy zapytanie oraz zapisujemy rezultat w obiekcie typu ResultSet
-		ResultSet rs = stat.executeQuery("SELECT e.first_name, e.last_name, p.salary FROM employees e join payroll p using (employee_id)");
-		System.out.println("---------------------------------");
-		// iteracyjnie odczytujemy rezultaty zapytania
-		while (rs.next())
-			System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
-		System.out.println("---------------------------------");
+        Statement stat = conn.createStatement(); // Statement przechowujacy polecenie SQL
 
-		rs.close();
-		stat.close();
-	}
+        // wydajemy zapytanie oraz zapisujemy rezultat w obiekcie typu ResultSet
+        ResultSet rs = stat.executeQuery("SELECT e.first_name, e.last_name, p.salary FROM employees e join payroll p using (employee_id)");
+        System.out.println("---------------------------------");
+        // iteracyjnie odczytujemy rezultaty zapytania
+        while (rs.next())
+            System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
+        System.out.println("---------------------------------");
+
+        rs.close();
+        stat.close();
+    }
 }
